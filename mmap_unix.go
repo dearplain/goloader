1a3dc56ec4b1da4062b7ea476c8cadf0fe01bc07
dearplain <lw6c@qq.com>
2018-03-31 00:30:16 +0800

    mmap

--- mmap_unix.go
+++ mmap_unix.go
@@ -2,17 +2,28 @@
 
 package goloader
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+)
 
 func mmap(size int) ([]byte, error) {
-	return syscall.Mmap(
-		-1,
+	data, err := syscall.Mmap(
+		0,
 		0,
 		size,
 		syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC,
-		syscall.MAP_ANON)
+		syscall.MAP_PRIVATE|syscall.MAP_ANON)
+	if err != nil {
+		err = os.NewSyscallError("syscall.Mmap", err)
+	}
+	return data, err
 }
 
 func munmap(b []byte) (err error) {
-	return syscall.Munmap(b)
+	err = syscall.Munmap(b)
+	if err != nil {
+		err = os.NewSyscallError("syscall.Munmap", err)
+	}
+	return
 }
